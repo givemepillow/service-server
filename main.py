5be977703914c83d64b9c7d44eb55d3117d0f522
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-02 02:03:24 +0300

    Added getting db info from .env.

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import asyncio
 
+import envfileparser
+
 from database import Connection, Database
 from server import Server
 
-user = 'kirill'
-password = '41234123'
-database = 'messenger'
-host = '127.0.0.1'
-
 
 async def main():
+    envs = envfileparser.get_env_from_file()
+
     # Creating connection with database.
-    await Connection.connect(user, password, database, host)
+    await Connection.connect(envs['USER'], envs['PASSWORD'], envs['DB'], envs['HOST'])
 
     # Init db with connection - creating tables if they not exists.
     await Database.initialization(Connection.connection())
