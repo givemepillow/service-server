e29c10060a9db5c22ac0abe6a6a877dd1fb73db4
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-07 20:20:55 +0300

    Init server implementation.

--- server/Server.py
+++ server/Server.py
@@ -1,14 +1,18 @@
 import asyncio
 
+from .Handler import Handler
+
 
 class Server:
-
     __buffer_size = 1024
 
     @classmethod
     async def handle(cls, reader, writer):
         data = await reader.read(cls.__buffer_size)
-        message = data.decode()
+        request = data.decode('utf-8')
+        answer = await Handler.handle_request(request)
+        writer.write(answer)
+        await writer.drain()
         writer.close()
 
     @classmethod
@@ -19,7 +23,7 @@ class Server:
     async def main(cls):
         server = await asyncio.start_server(cls.handle, '127.0.0.1', 6767)
 
-        address = server.sockets[0].getsockname()
+        # address = server.sockets[0].getsockname()
 
         async with server:
             await server.serve_forever()
