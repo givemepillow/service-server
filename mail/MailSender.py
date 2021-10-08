d631101a58a0a8b8d1d78335d79c8573cf6e408e
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-08 21:44:53 +0300

    Created mail sender class.

---
+++ mail/MailSender.py
@@ -0,0 +1,23 @@
+from .MailService import MailService
+from .MailBuilder import MailBuilder
+
+
+class MailSender:
+    service: MailService
+    INSTANCE = False
+
+    def __init__(self):
+        self.service = MailService()
+
+    @classmethod
+    def create(cls):
+        if cls.INSTANCE:
+            raise Exception('Singleton mail service!')
+        cls.INSTANCE = True
+        return MailSender()
+
+    def __call__(self, destination, subject, body):
+        return self.service.service.users().messages().send(
+            userId="me",
+            body=MailBuilder.create(destination, subject, body)
+        ).execute()
