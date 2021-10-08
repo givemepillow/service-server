965e1e5cf63c3542c6f87e68a20683e449234b22
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-08 03:15:52 +0300

    Exceptions messages support added.

--- mail/Mail.py
+++ mail/Mail.py
@@ -8,15 +8,18 @@ class Mail:
             self.__server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
             self.__server.ehlo()
             self.__server.login(user, password)
-        except smtplib.SMTPAuthenticationError as e:
-            print(e)
-        except ConnectionRefusedError as e:
-            print(e)
+        except smtplib.SMTPAuthenticationError:
+            raise Exception("Ошибка аутентификации почтового сервера :(")
+        except ConnectionRefusedError:
+            raise Exception("Ошибка установик соединения с почтовым сервером :(")
 
     def send(self, to, mail_text):
-        mail_text.set_from(self.__from)
-        mail_text.set_to(to)
-        self.__server.sendmail(self.__from, to, mail_text.get_text())
+        try:
+            mail_text.set_from(self.__from)
+            mail_text.set_to(to)
+            self.__server.sendmail(self.__from, to, mail_text.get_text())
+        except smtplib.SMTPRecipientsRefused:
+            raise Exception("Не удалось отправить код по указанному адресу :(")
 
 
 mail_instance = Mail('kirilllapushinskiy.bot@gmail.com', '2002K3r1llk4#BOT')
