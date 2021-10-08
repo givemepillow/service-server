6f5b56191d2b6c158b44d62db4b429f767ba76d8
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-09 01:53:53 +0300

    sql files removed.

--- sql/create/authentication_data.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE authentication_data;
-CREATE TABLE authentication_data(
-    password_hash TEXT NOT NULL ,
-    user_id INT NOT NULL UNIQUE ,
-    CONSTRAINT unique_user_id FOREIGN KEY(user_id) REFERENCES users(user_id)
-);
-
