9b0562ef4e146e7519b6426400fdfb0c6509c700
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-08 21:38:17 +0300

    Created mail service class.

--- mail/MailService.py
+++ mail/MailService.py
@@ -1,27 +1,26 @@
-import smtplib
-
-from .MailText import MailText
+from __future__ import print_function
+import os.path
+import pickle
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 
 class MailService:
-    def __init__(self, user, password):
-        try:
-            self.__from = user
-            self.__server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-            self.__server.ehlo()
-            self.__server.login(user, password)
-        except smtplib.SMTPAuthenticationError:
-            raise Exception("Ошибка аутентификации почтового сервера :(")
-        except ConnectionRefusedError:
-            raise ConnectionRefusedError("Ошибка установик соединения с почтовым сервером :(")
+    SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+    service = None
 
-    async def send(self, to, mail_text: MailText):
-        try:
-            mail_text.set_from(self.__from)
-            mail_text.set_to(to)
-            self.__server.sendmail(self.__from, to, mail_text.get_text())
-        except smtplib.SMTPRecipientsRefused:
-            raise smtplib.SMTPRecipientsRefused("Не удалось отправить код по указанному адресу :(")
-
-    def create_mail(self, subject, body):
-        return MailText(subject, body)
+    def __init__(self):
+        credentials = None
+        if os.path.exists("token.pickle"):
+            with open("token.pickle", "rb") as token:
+                credentials = pickle.load(token)
+        if not credentials or not credentials.valid:
+            if credentials and credentials.expired and credentials.refresh_token:
+                credentials.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', self.SCOPES)
+                credentials = flow.run_local_server(port=0)
+            with open("token.pickle", "wb") as token:
+                pickle.dump(credentials, token)
+        self.service = build('gmail', 'v1', credentials=credentials)
