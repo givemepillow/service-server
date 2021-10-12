e5695b87acaa9109d358a2b11a238300a079b0c9
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-12 14:23:03 +0300

    New exception handling.

--- mail/MailSender.py
+++ mail/MailSender.py
@@ -21,13 +21,12 @@ class MailSender:
         cls.INSTANCE = True
         return MailSender()
 
-    @logger.catch
     async def send(self, destination, subject, body):
         try:
             self.service.service.users().messages().send(
                 userId="me",
                 body=MailBuilder.create(destination, subject, body, '')
             ).execute()
-            logger.info(f"Отправлен код по адресу: {destination}")
         except googleapiclient.errors.HttpError:
-            logger.error(f"Не удалось отпраить электронное сообщение по адресу {destination}")
+            error_message = f"Не удалось отпраить электронное сообщение по адресу {destination}"
+            raise ValueError(error_message)
