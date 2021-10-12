ef57d498fe14a48684a89b35da70e33ca2274995
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-12 14:23:29 +0300

    Added email authentication.

--- database/Database.py
+++ database/Database.py
@@ -3,7 +3,7 @@ import asyncpg
 
 class Database:
     __connection: asyncpg.Connection
-    REGISTRATION_QUERY = 'INSERT INTO users(login, email, first_name, last_name) VALUES ($1, $2, $3, $4)'
+    REGISTRATION_QUERY = 'INSERT INTO users(login, email, first_name, last_name, password) VALUES ($1, $2, $3, $4, $5)'
     LOGIN_QUERY = 'SELECT login FROM users WHERE login = $1'
     EMAIL_QUERY = 'SELECT email FROM users WHERE email = $1'
 
@@ -13,10 +13,9 @@ class Database:
 
     @classmethod
     async def authentication(cls, login, email, password):
-        result = await cls.__connection.fetch(cls.LOGIN_QUERY, login)
-        if len(result) == 0:
-            return False
-        return True if login == result[0]['login'] else False
+        if await cls.exists_email(email) or await cls.exists_login(login):
+            return True
+        return False
 
     @classmethod
     async def exists_email(cls, email):
@@ -37,7 +36,7 @@ class Database:
         try:
             await cls.__connection.fetch(
                 cls.REGISTRATION_QUERY,
-                login, email, first_name, last_name
+                login, email, first_name, last_name, password
             )
             return True
         except asyncpg.exceptions.CheckViolationError or asyncpg.exceptions.UniqueViolationError as e:
