7182f7f87a989483e3458fe09e40aaef76f151e7
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-12 14:31:38 +0300

    Encryption key handler.

--- core/handlers/handlers.py
+++ core/handlers/handlers.py
@@ -2,11 +2,14 @@ from .authentication import authentication
 from .registration import registration
 from .email_verification import email_verification
 from .code_verification import code_verification
-from core.data_model import RequestType
+from .encryption_key import encryption_key
+from core.types import RequestType
 
 handlers = {
     RequestType.AUTHENTICATION: authentication,
     RequestType.REGISTRATION: registration,
     RequestType.EMAIL_VERIFICATION: email_verification,
-    RequestType.CODE_VERIFICATION: code_verification
+    RequestType.CODE_VERIFICATION: code_verification,
+    RequestType.ENCRYPTION_KEY: encryption_key
+
 }
