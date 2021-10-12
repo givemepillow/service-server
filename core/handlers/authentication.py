2dede91cd30e32d2fea5de1e6b97f355a6f58d30
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-12 14:32:09 +0300

    Added Cryptographer decrypt.

--- core/handlers/authentication.py
+++ core/handlers/authentication.py
@@ -1,14 +1,35 @@
+from loguru import logger
+
 from core.converters import AnswerConstructor
+from core.security import Cryptographer
 from database import Database
-from core.data_model import AnswerType
+from core.types import AnswerType
 
 
 async def authentication(request):
+    try:
+        password = Cryptographer.decrypt(request.data.password)
+    except ValueError:
+        logger.warning(
+            f"Ошибка расшифровки пароля для пользователя "
+            f"{request.data.login or request.data.email}:"
+            f" {request.ip}"
+        )
+        return AnswerConstructor.create(AnswerType.ERROR, message='Ошибка расшифровки пароля.')
     db_answer = await Database.authentication(
-        request.data.login,
-        request.data.password
+        login=request.data.login,
+        email=request.data.email,
+        password=password
     )
     if db_answer:
-        return AnswerConstructor.create(AnswerType.ACCEPT)
+        logger.info(
+            f"Подтверждена аутентификация "
+            f"{request.data.login or request.data.email}: "
+            f"{request.ip}")
+        return AnswerConstructor.create(AnswerType.ACCEPT, info='Вход подтверждён.')
     else:
-        return AnswerConstructor.create(AnswerType.REJECT)
+        logger.info(
+            f"Отклонена аутентификация "
+            f"{request.data.login or request.data.email}: "
+            f"{request.ip}")
+        return AnswerConstructor.create(AnswerType.REJECT, cause='Неверный логин или пароль.')
--- core/handlers/registration.py
+++ core/handlers/registration.py
@@ -1,21 +1,38 @@
+from loguru import logger
+
+from core.security import Cryptographer
 from database import Database
 from core.converters import AnswerConstructor
-from core.data_model import AnswerType
+from core.types import AnswerType
 from core.verification import Verify
 
 
 async def registration(request):
-    if await Verify.is_verified_email(request.data.email):
+    if await Verify.is_verified(request.data.email, request.data.login):
+        try:
+            encrypted_password = Cryptographer.decrypt(request.data.password).decode()
+        except ValueError:
+            logger.warning(
+                f"Ошибка расшифровки пароля для пользователя "
+                f"{request.data.login or request.data.email}:"
+                f" {request.ip}"
+            )
+            return AnswerConstructor.create(AnswerType.ERROR, message='Ошибка расшифровки пароля.')
         db_answer = await Database.registration(
-            request.data.login,
-            request.data.password,
-            request.data.first_name,
-            request.data.last_name,
-            request.data.email
+            login=request.data.login,
+            password=encrypted_password,
+            first_name=request.data.first_name,
+            last_name=request.data.last_name,
+            email=request.data.email
         )
         if db_answer:
-            return AnswerConstructor.create(AnswerType.ACCEPT)
+            logger.info(f"Зарегистрирован новый пользователь {request.data.email}: {request.ip}")
+            return AnswerConstructor.create(AnswerType.ACCEPT, info='Регистрация завершена.')
         else:
-            return AnswerConstructor.create(AnswerType.REJECT)
+            logger.info(
+                f'Отклонена регистрация нового пользователя (Недопустимые данные.) {request.data.email}: {request.ip}')
+            return AnswerConstructor.create(AnswerType.REJECT, cause='Недопустимые данные.')
     else:
-        return AnswerConstructor.create(AnswerType.REJECT)
+        logger.warning(
+            f'Отклонена регистрация нового пользователя (Почта или логин не подтверждены.) {request.data.email}: {request.ip}')
+        return AnswerConstructor.create(AnswerType.REJECT, cause='Почта или логин не подтверждены.')
