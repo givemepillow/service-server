e342eedf158f5ed6853981a2707ef18047aa916a
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-12 14:31:27 +0300

    Encryption key handler.

--- core/handlers/encryption_key.py
+++ core/handlers/encryption_key.py
@@ -1,11 +1,13 @@
 from loguru import logger
 
 from core.converters import AnswerConstructor
-from core.data_model import AnswerType
+from core.types import AnswerType
 from core.security import Cryptographer
+
+__all__ = ['encryption_key']
 
 
 async def encryption_key(request):
     answer = AnswerConstructor.create(AnswerType.KEY, key=Cryptographer.get_public_key())
-    logger.info(f"Выдан публичный ключ по адресу: {request.ip}")
+    logger.info(f'Выдан публичный ключ для {request.data.email}: {request.ip}')
     return answer
