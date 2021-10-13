498759bb1bd7605b1ac1d60b709b5e2d7202c3ad
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-13 22:04:30 +0300

    Database queries refactoring. Added get_password().

--- database/Database.py
+++ database/Database.py
@@ -1,9 +1,11 @@
 import asyncpg
+from loguru import logger
 
 
 class Database:
     __connection: asyncpg.Connection
-    REGISTRATION_QUERY = 'INSERT INTO users(login, email, first_name, last_name, password) VALUES ($1, $2, $3, $4, $5)'
+    REGISTRATION_QUERY = 'INSERT INTO users(login, email, first_name, last_name, password_hash) ' \
+                         'VALUES ($1, $2, $3, $4, $5)'
     LOGIN_QUERY = 'SELECT login FROM users WHERE login = $1'
     EMAIL_QUERY = 'SELECT email FROM users WHERE email = $1'
 
@@ -12,24 +14,26 @@ class Database:
         cls.__connection = connection
 
     @classmethod
-    async def authentication(cls, login, email, password):
-        if await cls.exists_email(email) or await cls.exists_login(login):
-            return True
-        return False
+    async def get_password(cls, email, login):
+        if email:
+            result = await cls.__connection.fetch('SELECT password_hash FROM users WHERE email = $1', email)
+        else:
+            result = await cls.__connection.fetch('SELECT password_hash FROM users WHERE login = $1', login)
+        return result[0][0]
 
     @classmethod
     async def exists_email(cls, email):
         result = await cls.__connection.fetch(cls.EMAIL_QUERY, email)
         if len(result) == 0:
-            return True
-        return False
+            return False
+        return True
 
     @classmethod
     async def exists_login(cls, login):
         result = await cls.__connection.fetch(cls.LOGIN_QUERY, login)
         if len(result) == 0:
-            return True
-        return False
+            return False
+        return True
 
     @classmethod
     async def registration(cls, login, password, first_name, last_name, email):
@@ -39,6 +43,10 @@ class Database:
                 login, email, first_name, last_name, password
             )
             return True
-        except asyncpg.exceptions.CheckViolationError or asyncpg.exceptions.UniqueViolationError as e:
-            print(e)
-            return False
+        except asyncpg.exceptions.CheckViolationError as e:
+            logger.exception(e)
+        except asyncpg.exceptions.UndefinedColumnError as e:
+            logger.exception(e)
+        except asyncpg.exceptions.UniqueViolationError as e:
+            logger.exception(e)
+        return False
