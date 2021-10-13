8f8f93ba5a32417b17195a964295f37e29e04725
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-13 22:05:58 +0300

    Reject for not exists login.

--- core/handlers/encryption_key.py
+++ core/handlers/encryption_key.py
@@ -6,8 +6,14 @@ from core.security import Cryptographer
 
 __all__ = ['encryption_key']
 
+from database import Database
+
 
 async def encryption_key(request):
-    answer = AnswerConstructor.create(AnswerType.KEY, key=Cryptographer.get_public_key())
-    logger.info(f'Выдан публичный ключ для {request.data.email}: {request.ip}')
+    if request.data.login and not await Database.exists_login(request.data.login):
+        answer = AnswerConstructor.create(AnswerType.REJECT, cause='Несуществующий логин.')
+        logger.info(f'Попытка получения ключа шифрования по несуществующему логину {request.data.login}: {request.ip}')
+    else:
+        answer = AnswerConstructor.create(AnswerType.KEY, key=Cryptographer.get_public_key())
+        logger.info(f'Выдан публичный ключ для {request.data.email or request.data.email}: {request.ip}')
     return answer
