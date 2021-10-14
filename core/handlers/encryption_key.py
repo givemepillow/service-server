4ad0153407d9402cb2772c5badce4a67856b6083
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-14 20:36:31 +0300

    Added email exists verification.

--- core/handlers/encryption_key.py
+++ core/handlers/encryption_key.py
@@ -12,8 +12,14 @@ from database import Database
 async def encryption_key(request):
     if request.data.login and not await Database.exists_login(request.data.login):
         answer = AnswerConstructor.create(AnswerType.REJECT, cause='Несуществующий логин.')
-        logger.info(f'Попытка получения ключа шифрования по несуществующему логину {request.data.login}: {request.ip}')
+        logger.info(f'Попытка получения ключа шифрования по несуществующему логину '
+                    f'{request.data.login}: {request.ip}')
+    elif request.data.email and not await Database.exists_email(request.data.email):
+        answer = AnswerConstructor.create(AnswerType.REJECT, cause='Несуществующий адрес электронной почты.')
+        logger.info(f'Попытка получения ключа шифрования по несуществующему адресу электронной почты '
+                    f'{request.data.login}: {request.ip}')
     else:
         answer = AnswerConstructor.create(AnswerType.KEY, key=Cryptographer.get_public_key())
-        logger.info(f'Выдан публичный ключ для {request.data.email or request.data.login}: {request.ip}')
+        logger.info(f'Выдан публичный ключ для '
+                    f'{request.data.email or request.data.login}: {request.ip}')
     return answer
