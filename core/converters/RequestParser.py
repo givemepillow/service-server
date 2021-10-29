4e0dff81bdaecaa3f1e4c78919d3038e696bfee7
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-29 12:38:28 +0300

    Request parsing by type field.

--- core/converters/RequestParser.py
+++ core/converters/RequestParser.py
@@ -1,14 +1,19 @@
 import json
 from json import JSONDecodeError
 
-from core.types import Request
+from core.types import Request, requests, RequestType
 
 
 class RequestParser:
     @classmethod
     def extract_request(cls, request) -> Request:
+        json_request = json.loads(request)
         try:
-            request = Request.parse_obj(json.loads(request))
+            parsed_request = Request(
+                type=json_request['type'],
+                data=requests[RequestType(json_request['type'])].parse_obj(json_request['data']),
+                ip=None
+            )
         except JSONDecodeError:
             raise ValueError('Получен невалидный запрос.')
-        return request
+        return parsed_request
