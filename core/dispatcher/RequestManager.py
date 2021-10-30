ad6f977884f6224de1abcfceed66b5aa5f617efd
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-10-30 17:21:32 +0300

    Select handler by RequestType.

--- core/dispatcher/RequestManager.py
+++ core/dispatcher/RequestManager.py
@@ -1,10 +1,8 @@
-from json import JSONDecodeError
-
 from loguru import logger
 
 from core.converters import RequestParser, ResponseConstructor
 from core.handlers import handlers
-from core.types import ResponseType
+from core.types import ResponseType, RequestType
 
 
 class RequestManager:
@@ -18,9 +16,9 @@ class RequestManager:
             return bytes(ResponseConstructor.create(ResponseType.ERROR, message="Невалидный запрос."), encoding="utf-8")
 
         try:
-            to_send = await handlers[request.type](request)
+            to_send = await handlers[RequestType(request.type)](request)
         except KeyError:
-            logger.warning('Получен неизвестный тип запроса.')
+            logger.warning('Получен неизвестный тип запроса. Нет соответствующего хендлера.')
             to_send = ResponseConstructor.create(ResponseType.ERROR, message="Запрос не распознан.")
 
         return bytes(to_send, encoding="utf-8")
