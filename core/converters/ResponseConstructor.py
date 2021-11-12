27f9cdeab0cf9ba0c111197f68e66069a5d95d51
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-11-12 23:46:20 +0300

    Refactoring.

--- core/converters/ResponseConstructor.py
+++ core/converters/ResponseConstructor.py
@@ -4,11 +4,11 @@ from core.types.Responses import Response, responses
 class ResponseConstructor:
 
     @classmethod
-    def create(cls, answer_type, **kwargs):
+    def create(cls, response_type, **kwargs):
         data = dict()
         for field in kwargs:
             data[field] = kwargs[field]
         return Response.construct(
-            type=answer_type,
-            data=responses[answer_type].parse_obj(data)
+            type=response_type,
+            data=responses[response_type].parse_obj(data)
         ).json()
