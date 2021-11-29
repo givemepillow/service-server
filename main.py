e82b7c0bd7f8326a6756c65c316104fd7b33b016
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2021-11-30 01:33:42 +0300

    Server config in .env.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import asyncio
 
+import envfileparser
+
 from loader import DatabaseLoader, MailLoader, LoggerLoader, CryptographerLoader, StatisticLoader
 from server import Server
 
 
 async def main():
+    envs = envfileparser.get_env_from_file()
     LoggerLoader.start()
     CryptographerLoader.start()
     await DatabaseLoader.start()
     await MailLoader.start()
     await StatisticLoader.start()
-    await Server.start()
+    await Server.start(port=int(envs['SERVER_PORT']), address=envs['SERVER_IP'])
 
 
 loop = asyncio.get_event_loop()
